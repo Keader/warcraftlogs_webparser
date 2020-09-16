f0872f4f28059246dc0a42730628c87161a6ad76
Keader <keader.android@gmail.com>
2020-09-16 11:47:11 -0300

    Getting all pages of some url + filtering only wotlk logs

--- src/crawler.js
+++ src/crawler.js
@@ -1,25 +1,48 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
+const moment = require('moment');
 
-exports.GetAllLogsLinks = async function(baseUrl) {
+// wotlk duration
+const minDate = moment("13-11-2008", "DD-MM-YYYY");
+const maxDate = moment("12-10-2010", "DD-MM-YYYY");
+
+exports.GetAllLogsLinks = async function (baseUrl) {
+
+    const maxPages = await this.GetAllLogsLinksPerPage(baseUrl);
+    for (let i = 2; i < maxPages; ++i)
+    {
+        await this.GetAllLogsLinksPerPage(baseUrl+`p${i}`);
+    }
+}
+
+exports.GetAllLogsLinksPerPage = async function(baseUrl) {
     const body = await axios.get(baseUrl);
     const $ = cheerio.load(body.data);
 
     const mainTable = $("div[class='ui-widget ui-widget-content ui-corner-all']");
     const pagination = $('.pagination').find('li');
     const lastPageUrl = $(pagination[pagination.length - 1].firstChild).attr('href');
+    let lastPageIndex = 1;
+    if (lastPageUrl.includes('/p'))
+        lastPageIndex = lastPageUrl.split('/p')[1].replace('/', '');
 
     // Tables with data/url
     const tableList = mainTable.find('tr');
     for (let i = 1; i < tableList.length; i++) {
-        console.log("--------------------------------");
         const tableElement = tableList[i].children.filter((element) => element.data != "\n\t\t" && element.data != "\n\t");
 
-        let url = $(tableElement[1].firstChild).attr('href');
-        let data = $(tableElement[1].firstChild).text();
-        console.log(`${data}, ${url}`);
+        const url = $(tableElement[1].firstChild).attr('href');
+        const date = $(tableElement[1].firstChild).text();
+        const dateMoment = moment(date, "DD MMM YYYY");
+        const dateString = dateMoment.format('DD-MM-YYYY');
+
+        // check if log is from wotlk
+        if (dateMoment.isBefore(minDate) || dateMoment.isAfter(maxDate))
+            continue;
+
+        console.log("--------------------------------");
+        console.log(`${date}, ${url}, ${dateString}`);
     }
 
-    console.log(`--------------------------------`);
-    console.log(`Ultima pagina eh: ${lastPageUrl}`);
+   return lastPageIndex;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const crawler = require('./crawler.js');
 
 const baseUrl10Man = "http://worldoflogs.com/zone/eu-us/icecrown-citadel/lord-marrowgar/normal-10/kills/";
+
 crawler.GetAllLogsLinks(baseUrl10Man);
+
